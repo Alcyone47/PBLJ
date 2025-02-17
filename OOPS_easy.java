8f0a70c3043a12c294160c3a31ad48a0accfe567
Alcyone47 <91411013+Alcyone47@users.noreply.github.com>
2025-02-17 10:23:29 +0530

    expt2

---
+++ OOPS_easy.java
@@ -0,0 +1,12 @@
+/* Problem Statement: Write a Java program to create a Product class with attributes id, name, and price. The program should:
+Demonstrate the use of constructors and methods to display product details
+Input Example:
+Product ID: 101
+Name: Laptop
+Price: 75000
+Output Example:
+Product Details:
+ID: 101
+Name: Laptop
+Price: 75000 */
+
